25e1919666cb827577040c3a8e3bea548bef41e6
AndreaFerrante <andreaferrante1986@gmail.com>
2024-08-17 10:44:47 +0200

    Test new UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react'
-import ChatInterface from './components/chat';
-import FileUpload from './components/upload';
-import { Box, Typography } from '@mui/material';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => {
-    const [isUploaded, setIsUploaded] = useState(false);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-    const handleUpload = () => {
-        setIsUploaded(true);
-    };
-
-    return (
-        <Box sx={{ padding: '20px' }}>
-            <Typography variant="h4" sx={{ textAlign: 'center' }}>Chat with your PDF Documents</Typography>
-            {!isUploaded && <FileUpload onUpload={handleUpload} />}
-            {isUploaded && <ChatInterface />}
-        </Box>
-    );
-};
-
-export default App;
+export default App
